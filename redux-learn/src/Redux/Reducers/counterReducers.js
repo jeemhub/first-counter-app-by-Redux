e37991804b71d9dfbcbd3003e175e46c5d34d630
Vleo <jassim.vleo@gmail.com>
2022-07-25 21:43:56 +0300

    Action creator

--- redux-learn/src/Redux/Reducers/counterReducers.js
+++ redux-learn/src/Redux/Reducers/counterReducers.js
@@ -1,9 +1,11 @@
-import { DECREMENT, INCREMENT } from "../Action/Type";
+import { ADDNUM, DATA, DECREMENT, INCREMENT } from "../Action/Type";
 
-const counterReducers = (state = {counter:0,}, action) => {
+const counterReducers = (state = {counter:0,data:'',}, action) => {
     switch (action.type) {
         case INCREMENT: return ({ ...state, counter: state.counter + 1 });
         case DECREMENT: return ({ ...state, counter: state.counter - 1 });
+        case DATA: return ({ ...state, data: action.data});
+        case ADDNUM: return ({ ...state, counter: state.counter + +action.data});
         default: return state;
     }
 }
